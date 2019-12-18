7ccee70e2eaca9ac502e499673ac2477a1118184
masaki-hirose <36248899+masaki-hirose@users.noreply.github.com>
2019-12-18 14:53:36 +0900

    Create dm_exec_query_stats_insert.sql
    直近1分間に実行されたクエリ情報を取得してテーブルにダンプ（1分間に1回実行を推奨）

---
+++ DMVダンプクエリ/dm_exec_query_stats_insert.sql
@@ -0,0 +1,29 @@
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+insert into dm_exec_query_stats_dump
+SELECT
+  getdate() as collect_date
+  ,qt.dbid
+  ,qt.TEXT as parent_query
+  ,SUBSTRING(qt.TEXT, qs.statement_start_offset / 2, (
+      CASE 
+        WHEN qs.statement_end_offset = - 1
+          THEN LEN(CONVERT(NVARCHAR(MAX), qt.TEXT)) * 2
+        ELSE qs.statement_end_offset
+      END - qs.statement_start_offset
+   ) / 2) as statement
+  ,execution_count
+  ,total_worker_time
+  ,total_elapsed_time
+  ,total_physical_reads
+  ,total_logical_reads
+  ,total_logical_writes
+  ,total_dop
+  ,min_dop
+  ,max_dop
+  ,last_execution_time
+  ,creation_time
+FROM sys.dm_exec_query_stats qs
+OUTER APPLY sys.dm_exec_sql_text(qs.sql_handle) AS qt
+WHERE
+   last_execution_time > DATEADD(minute, -1, getdate())
+or creation_time > DATEADD(minute, -1, getdate())
