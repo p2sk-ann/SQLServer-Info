157e21ccba08a855ee84fe66a526c6b162a76779
masaki-hirose <36248899+masaki-hirose@users.noreply.github.com>
2020-06-02 18:35:47 +0900

    Create dm_db_index_operational_stats_dump_insert.sql

---
+++ DMVダンプクエリ/dm_db_index_operational_stats（インデックスごとのアクセス状況）/dm_db_index_operational_stats_dump_insert.sql
@@ -0,0 +1,9 @@
+insert into dm_db_index_operational_stats_dump
+select 
+	 getdate() as collect_date
+	,object_name(i.object_id) as table_name
+	,i.name
+	,d.*
+from sys.dm_db_index_operational_stats(db_id(), null, null, null) d
+left join sys.indexes i on d.OBJECT_ID = i.OBJECT_ID
+	and d.index_id = i.index_id
