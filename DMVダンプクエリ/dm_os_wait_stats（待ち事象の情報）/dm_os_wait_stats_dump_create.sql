27d63d1c2767f9f255c2ddc50b7b9c2617edac10
masaki-hirose <36248899+masaki-hirose@users.noreply.github.com>
2020-06-02 21:19:41 +0900

    Update dm_os_wait_stats_dump_create.sql

--- DMVダンプクエリ/dm_os_wait_stats（待ち事象の情報）/dm_os_wait_stats_dump_create.sql
+++ DMVダンプクエリ/dm_os_wait_stats（待ち事象の情報）/dm_os_wait_stats_dump_create.sql
@@ -2,3 +2,6 @@ select getdate() as collect_date, *
 into dm_os_wait_stats_dump
 from sys.dm_os_wait_stats with(nolock)
 where 1=0
+
+--古いデータ削除用
+create index IX_dm_os_wait_stats_dump_collect_date on dm_os_wait_stats_dump(collect_date)
