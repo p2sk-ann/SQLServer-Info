be7df489a74484a659eb6ddb53fb927760f6acea
masaki-hirose <36248899+masaki-hirose@users.noreply.github.com>
2020-06-02 21:04:46 +0900

    Create dm_os_waiting_tasks_dump_insert.sql

---
+++ DMVダンプクエリ/dm_os_waiting_tasks（現在実行中タスクに紐づいた待ち事象）/dm_os_waiting_tasks_dump_insert.sql
@@ -0,0 +1,31 @@
+insert into dm_os_waiting_tasks_dump
+select top 100
+   getdate() as collect_date
+  ,wt.session_id
+  ,es.program_name
+  ,es.host_name
+  ,wt.blocking_session_id
+  ,er.blocking_session_id as er_blocking_session_id
+  ,wt.exec_context_id
+  ,er.start_time
+  ,er.wait_time
+  ,wt.wait_duration_ms
+  ,er.status
+  ,er.command
+  ,wt.wait_type
+  ,er.wait_type as er_wait_type
+  ,er.last_wait_type
+  ,wt.resource_description
+  ,er.wait_resource
+  ,wt.blocking_exec_context_id
+  ,ib.event_info
+  ,ib.event_type
+  ,ib.parameters
+from sys.dm_os_waiting_tasks as wt with (nolock)
+left join sys.dm_exec_requests as er with (nolock) on er.session_id = wt.session_id
+left join sys.dm_exec_sessions as es with (nolock) on es.session_id = wt.session_id
+outer apply sys.dm_exec_input_buffer(wt.session_id, null) as ib
+where wt.session_id > 50
+  and wt.wait_duration_ms >= 100
+  and er.status <> 'background'
+option (maxdop 1)
