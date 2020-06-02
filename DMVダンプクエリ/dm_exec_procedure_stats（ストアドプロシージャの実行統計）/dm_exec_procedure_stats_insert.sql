9fcad27033bdb52f0f649423d9b9629fa039b937
masaki-hirose <36248899+masaki-hirose@users.noreply.github.com>
2020-06-02 20:52:15 +0900

    Create dm_exec_procedure_stats_insert.sql

---
+++ DMVダンプクエリ/dm_exec_procedure_stats（ストアドプロシージャの実行統計）/dm_exec_procedure_stats_insert.sql
@@ -0,0 +1,34 @@
+insert into dm_exec_procedure_stats_dump
+select top 500
+  getdate() as collect_date,
+  object_name(ps.object_id, ps.database_id) as object_name,
+  ps.last_execution_time,
+  o.modify_date,
+  ps.cached_time,
+  ps.execution_count,
+  ps.total_worker_time,
+  ps.last_worker_time,
+  ps.min_worker_time,
+  ps.max_worker_time,
+  ps.total_elapsed_time,
+  ps.last_elapsed_time,
+  ps.min_elapsed_time,
+  ps.max_elapsed_time,
+  ps.total_logical_writes,
+  ps.last_logical_writes,
+  ps.min_logical_writes,
+  ps.max_logical_writes,
+  ps.total_logical_reads,
+  ps.last_logical_reads,
+  ps.min_logical_reads,
+  ps.max_logical_reads,
+  ps.plan_handle,
+  text,
+  query_plan
+from
+  sys.dm_exec_procedure_stats  as ps
+  cross apply sys.dm_exec_sql_text(sql_handle)
+  cross apply sys.dm_exec_query_plan(plan_handle)
+  left join sys.objects as o on o.object_id = ps.object_id
+where last_execution_time >= dateadd(minute, -1, getdate())
+option(maxdop 1)
