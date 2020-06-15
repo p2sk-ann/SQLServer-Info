4d81ce523355ac552dc5bf91de782cc8cf1d3102
masaki-hirose <36248899+masaki-hirose@users.noreply.github.com>
2020-06-15 20:16:58 +0900

    Update dm_exec_requests_dump_insert.sql

--- DMVダンプクエリ/dm_exec_requests（現在実行中のクエリリスト）/dm_exec_requests_dump_insert.sql
+++ DMVダンプクエリ/dm_exec_requests（現在実行中のクエリリスト）/dm_exec_requests_dump_insert.sql
@@ -21,6 +21,9 @@ select top 100
   ,der.cpu_time
   ,(case der.transaction_isolation_level when 0 then 'Unspecified' when 1 then 'ReadUncomitted' when 2 then 'ReadCommitted' when 3 then 'Repeatable' when 4 then 'Serializable' when 5 then 'Snapshot' else cast(der.transaction_isolation_level as varchar) end) as transaction_isolation_level
   ,der.granted_query_memory * 8 as granted_query_memory_kb
+  ,der.reads
+  ,der.writes
+  ,der.logical_reads
 from sys.dm_exec_requests der
 join sys.dm_exec_sessions des on des.session_id = der.session_id
 outer apply sys.dm_exec_sql_text(sql_handle) as dest
