7be116dfcdcb74c07d354abbc1f791d150359a4c
masaki-hirose <36248899+masaki-hirose@users.noreply.github.com>
2020-06-15 20:04:11 +0900

    Update dm_exec_query_stats_dump_create.sql

--- DMVダンプクエリ/dm_exec_query_stats（キャッシュされたパフォーマンス情報）/dm_exec_query_stats_dump_create.sql
+++ DMVダンプクエリ/dm_exec_query_stats（キャッシュされたパフォーマンス情報）/dm_exec_query_stats_dump_create.sql
@@ -2,16 +2,16 @@ set transaction isolation level read uncommitted
 select
   getdate() as collect_date
   ,qt.dbid
-  --サイズが大きくなりすぎるので3000文字だけ格納
-  ,substring(qt.text, 1, 3000) as parent_query
-  --サイズが大きくなりすぎるので3000文字だけ格納
+  --サイズが大きくなりすぎるので500文字だけ格納
+  ,substring(qt.text, 1, 500) as parent_query
+  --サイズが大きくなりすぎるので500文字だけ格納
   ,substring(substring(qt.text, qs.statement_start_offset / 2, (
       case 
         when qs.statement_end_offset = - 1
           then len(convert(nvarchar(max), qt.text)) * 2
         else qs.statement_end_offset
       end - qs.statement_start_offset
-   ) / 2), 1, 3000) as statement
+   ) / 2), 1, 500) as statement
   ,execution_count
   ,total_worker_time
   ,total_elapsed_time
