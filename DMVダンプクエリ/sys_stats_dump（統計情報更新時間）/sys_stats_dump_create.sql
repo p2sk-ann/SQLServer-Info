e7d3b3a442f28bd6ac9d44f5af7dc2c96790ea22
masaki-hirose <36248899+masaki-hirose@users.noreply.github.com>
2020-06-15 19:43:32 +0900

    Update sys_stats_dump_create.sql

--- DMVダンプクエリ/sys_stats_dump（統計情報更新時間）/sys_stats_dump_create.sql
+++ DMVダンプクエリ/sys_stats_dump（統計情報更新時間）/sys_stats_dump_create.sql
@@ -1,4 +1,6 @@
-select object_name(s.object_id) as object_name
+select 
+   getdate as collect_date
+  ,object_name(s.object_id) as object_name
   ,s.name as statistics_name
   ,c.name as column_name
   ,sc.stats_column_id
@@ -10,3 +12,6 @@ inner join sys.stats_columns as sc on s.object_id = sc.object_id
 inner join sys.columns as c on sc.object_id = c.object_id
   and c.column_id = sc.column_id
 where 1=0
+
+--古いデータ削除用
+create index IX_sys_stats_dump_collect_date on sys_stats_dump(collect_date)
