b8a11e9ae4a95dd90a42cc5114965e6800d681f2
masaki-hirose <36248899+masaki-hirose@users.noreply.github.com>
2020-06-15 18:09:37 +0900

    Update sys_stats_dump_create.sql

--- DMVダンプクエリ/sys_stats_dump（統計情報更新時間）/sys_stats_dump_create.sql
+++ DMVダンプクエリ/sys_stats_dump（統計情報更新時間）/sys_stats_dump_create.sql
@@ -1,12 +1,12 @@
-SELECT object_name(s.object_id) AS object_name
-  ,s.NAME AS statistics_name
-  ,c.NAME AS column_name
+select object_name(s.object_id) as object_name
+  ,s.name as statistics_name
+  ,c.name as column_name
   ,sc.stats_column_id
-  ,STATS_DATE(s.object_id, s.stats_id) as statsdate --ここで統計情報の更新時間を確認できる
+  ,stats_date(s.object_id, s.stats_id) as statsdate --ここで統計情報の更新時間を確認できる
 into sys_stats_dump
-FROM sys.stats AS s
-INNER JOIN sys.stats_columns AS sc ON s.object_id = sc.object_id
-  AND s.stats_id = sc.stats_id
-INNER JOIN sys.columns AS c ON sc.object_id = c.object_id
-  AND c.column_id = sc.column_id
-WHERE 1=0
+from sys.stats as s
+inner join sys.stats_columns as sc on s.object_id = sc.object_id
+  and s.stats_id = sc.stats_id
+inner join sys.columns as c on sc.object_id = c.object_id
+  and c.column_id = sc.column_id
+where 1=0
