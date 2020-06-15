664d96cdb56ae1551ed1093bce0fc11235158d64
masaki-hirose <36248899+masaki-hirose@users.noreply.github.com>
2020-06-15 18:27:58 +0900

    Create dm_db_index_usage_stats_dump_insert.sql

---
+++ DMVダンプクエリ/dm_db_index_usage_stats（インデックスへのアクセス状況）/dm_db_index_usage_stats_dump_insert.sql
@@ -0,0 +1,24 @@
+insert into dm_db_index_usage_stats_dump
+select
+  getdate() as collect_date
+  ,object_name(sys.indexes.object_id) as table_name
+  ,sys.indexes.name as index_name
+  ,sys.dm_db_partition_stats.row_count as row_count
+  ,sys.dm_db_partition_stats.reserved_page_count * 8.0 / 1024 as size_mb
+  ,type_desc
+  ,sys.dm_db_index_usage_stats.*
+from
+  sys.dm_db_partition_stats
+left join sys.indexes on sys.dm_db_partition_stats.object_id = sys.indexes.object_id
+                      and sys.dm_db_partition_stats.index_id = sys.indexes.index_id
+left join sys.dm_db_index_usage_stats on sys.dm_db_partition_stats.object_id = sys.dm_db_index_usage_stats.object_id
+                                      and sys.dm_db_partition_stats.index_id = sys.dm_db_index_usage_stats.index_id
+where
+  last_user_seek > dateadd(minute, -3, getdate())
+  or last_user_scan > dateadd(minute, -3, getdate())
+  or last_user_lookup > dateadd(minute, -3, getdate())
+  or last_user_update > dateadd(minute, -3, getdate())
+  or last_system_seek > dateadd(minute, -3, getdate())
+  or last_system_scan > dateadd(minute, -3, getdate())
+  or last_system_lookup > dateadd(minute, -3, getdate())
+  or last_system_update > dateadd(minute, -3, getdate())
