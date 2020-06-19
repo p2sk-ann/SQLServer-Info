dafcabb48d39a85db4b90a2c30a7dbac997f7544
masaki-hirose <36248899+masaki-hirose@users.noreply.github.com>
2020-06-19 11:50:48 +0900

    Update sys_stats_dump_insert.sql

--- DMVダンプクエリ/sys_stats_dump（統計情報更新時間）/sys_stats_dump_insert.sql
+++ DMVダンプクエリ/sys_stats_dump（統計情報更新時間）/sys_stats_dump_insert.sql
@@ -1,20 +1,28 @@
 insert into sys_stats_dump
-select
-   getdate() as collect_date
-  ,object_name(s.object_id) as object_name
-  ,s.name as statistics_name
-  ,c.name as column_name
-  ,sc.stats_column_id
-  ,stats_date(s.object_id, s.stats_id) as statsdate --ここで統計情報の更新時間を確認できる
-from sys.stats as s
-inner join sys.stats_columns as sc on s.object_id = sc.object_id
-  and s.stats_id = sc.stats_id
+select 
+     getdate() as collect_date
+    ,object_name(s1.object_id) as object_name
+    ,s1.name as statistics_name
+    ,left(colname, len(colname) - 1) as column_list
+	,(case when charindex('|', left(colname, len(colname) - 1), 1) > 1 then 1 else 0 end) as multi_column_flag --1 : 複数列の統計情報
+    ,stats_date(s1.object_id, s1.stats_id) as statsdate --ここで統計情報の更新時間を確認できる
+from sys.stats as s1
+inner join sys.stats_columns as sc on s1.object_id = sc.object_id
+    and s1.stats_id = sc.stats_id and stats_column_id = 1
 inner join sys.columns as c on sc.object_id = c.object_id
-  and c.column_id = sc.column_id
-where exists (
-    select *
-    from sys.tables ss
-    where ss.name = object_name(s.object_id)
-    )
-and stats_date(s.object_id, s.stats_id) > dateadd(minute, -3, getdate())
+    and c.column_id = sc.column_id
+cross apply (
+    select
+      c.name + ' | ' 
+    from
+      sys.stats as s2
+    inner join sys.stats_columns as sc on s2.object_id = sc.object_id
+      and s2.stats_id = sc.stats_id
+    inner join sys.columns as c on sc.object_id = c.object_id
+      and c.column_id = sc.column_id
+    and s1.object_id = s2.object_id and s1.stats_id = s2.stats_id
+	order by stats_column_id
+for xml PATH('')
+) as a(colname)
+where stats_date(s1.object_id, s1.stats_id) > dateadd(minute, -2, getdate())
 option(maxdop 1)
