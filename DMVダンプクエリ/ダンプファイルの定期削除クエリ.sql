b765fd62989fb94c74e024d5be361562e8027d79
masaki-hirose <36248899+masaki-hirose@users.noreply.github.com>
2020-07-01 11:28:55 +0900

    Create ダンプファイルの定期削除クエリ.sql

---
+++ DMVダンプクエリ/ダンプファイルの定期削除クエリ.sql
@@ -0,0 +1,52 @@
+set transaction isolation level read uncommitted
+set lock_timeout 1000
+ 
+if exists (select * from sys.objects where name = 'dm_db_index_operational_stats_dump')
+begin
+    delete top (100000) from dm_db_index_operational_stats_dump where collect_date < dateadd(day, -10, getdate())
+end
+ 
+if exists (select * from sys.objects where name = 'dm_db_index_usage_stats_dump')
+begin
+    delete top (100000) from dm_db_index_usage_stats_dump where collect_date < dateadd(day, -10, getdate())
+end
+ 
+if exists (select * from sys.objects where name = 'dm_exec_procedure_stats_dump')
+begin
+    delete top (100000) from dm_exec_procedure_stats_dump where collect_date < dateadd(day, -10, getdate())
+end
+ 
+if exists (select * from sys.objects where name = 'dm_exec_query_stats_dump')
+begin
+    delete top (100000) from dm_exec_query_stats_dump where collect_date < dateadd(day, -10, getdate())
+end
+ 
+if exists (select * from sys.objects where name = 'dm_exec_requests_dump')
+begin
+    delete top (100000) from dm_exec_requests_dump where collect_date < dateadd(day, -10, getdate())
+end
+ 
+if exists (select * from sys.objects where name = 'dm_os_latch_stats_dump')
+begin
+    delete top (100000) from dm_os_latch_stats_dump where collect_date < dateadd(day, -10, getdate())
+end
+ 
+if exists (select * from sys.objects where name = 'dm_os_wait_stats_dump')
+begin
+    delete top (100000) from dm_os_wait_stats_dump where collect_date < dateadd(day, -10, getdate())
+end
+ 
+if exists (select * from sys.objects where name = 'dm_os_waiting_tasks_dump')
+begin
+    delete top (100000) from dm_os_waiting_tasks_dump where collect_date < dateadd(day, -10, getdate())
+end
+ 
+if exists (select * from sys.objects where name = 'sys_stats_dump')
+begin
+    delete top (100000) from sys_stats_dump where collect_date < dateadd(day, -10, getdate())
+end
+ 
+if exists (select * from sys.objects where name = 'dm_io_virtual_file_stats_dump')
+begin
+    delete top (100000) from dm_io_virtual_file_stats_dump where collect_date < dateadd(day, -10, getdate())
+end
