f03ec8383d89c88bab6410a2cae88b1ef04c5630
Masaki Hirose <36248899+masaki-hirose@users.noreply.github.com>
2020-08-21 14:46:51 +0900

    Create 調査用テーブル_create.sql

---
+++ 未使用オブジェクトの調査/未使用ストアド調査/調査用テーブル_create.sql
@@ -0,0 +1,22 @@
+--[dm_exec_procedure_stats_usage_dump]というテーブルを作成
+select
+  getdate() as first_insert_date,
+  object_name(ps.object_id, ps.database_id) as object_name,
+  db_name(ps.database_id) as database_name,
+  ps.last_execution_time,
+  o.modify_date as last_modify_date,
+  ps.cached_time as last_cached_time,
+  ps.execution_count as last_execution_count
+into dm_exec_procedure_stats_usage_dump
+from
+  sys.dm_exec_procedure_stats  as ps
+  left join sys.objects as o on o.object_id = ps.object_id
+where object_name(ps.object_id, ps.database_id) is not null
+and object_name(ps.object_id, ps.database_id) not like 'sp[_]MS%' --レプリ系除外
+order by cached_time asc
+
+--クラスタ化インデックス作成
+create clustered index CIX_dm_exec_procedure_stats_usage_dump on dm_exec_procedure_stats_usage_dump(object_name)
+
+--Unique制約作成
+alter table dm_exec_procedure_stats_usage_dump add constraint UQ_dm_exec_procedure_stats_usage_dump_object_name unique (object_name)   
