0f827a45160ddde1479178dd9ea2175e2c84ddc0
Masaki Hirose <36248899+masaki-hirose@users.noreply.github.com>
2020-10-23 16:57:57 +0900

    Create dm_exec_procedure_stats_dump_analysis_chronological.sql

---
+++ DMVダンプクエリ/dm_exec_procedure_stats（ストアドプロシージャの実行統計）/dm_exec_procedure_stats_dump_analysis_chronological.sql
@@ -0,0 +1,36 @@
+declare @start datetime, @end datetime
+set @start = '2020/10/13 00:00:00'
+set @end = '2020/10/23 00:00:00'
+
+select
+     a.object_name
+    ,a.collect_date
+    ,(a.execution_count - b.execution_count) as execution_count
+    ,(a.total_worker_time - b.total_worker_time) as total_worker_time
+    ,a.last_worker_time
+    ,a.min_worker_time
+    ,a.max_worker_time
+    ,(a.total_elapsed_time - b.total_elapsed_time) as total_elapsed_time
+    ,a.last_elapsed_time
+    ,a.min_elapsed_time
+    ,a.max_elapsed_time
+    ,(a.total_logical_writes - b.total_logical_writes) as total_logical_writes
+    ,a.last_logical_writes
+    ,a.min_logical_writes
+    ,a.max_logical_writes
+    ,(a.total_logical_reads - b.total_logical_reads) as total_logical_reads
+    ,a.last_logical_reads
+    ,a.min_logical_reads
+    ,a.max_logical_reads
+from
+(
+    select *, row_number() over (partition by object_name order by collect_date) as rownum from dm_exec_procedure_stats_dump
+) as a
+join
+(
+    select *, row_number() over (partition by object_name order by collect_date) as rownum from dm_exec_procedure_stats_dump
+) as b on a.object_name = b.object_name and a.rownum = b.rownum + 1 and a.cached_time = b.cached_time
+where a.collect_date between @start and @end
+order by
+     a.object_name
+    ,a.collect_date
