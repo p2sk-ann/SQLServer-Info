10a83b2b878a37390884b5643b880a57229acec7
Masaki Hirose <36248899+masaki-hirose@users.noreply.github.com>
2021-03-04 16:48:25 +0900

    Create dm_db_partition_stats_dump_create.sql

---
+++ DMVダンプクエリ/dm_db_partition_stats_dump（データサイズ）/dm_db_partition_stats_dump_create.sql
@@ -0,0 +1,9 @@
+select 
+ getdate() as collect_date
+,sum(used_page_count) as sum_used_page_count -- reservedの中で実際にデータが書き込まれているサイズ
+,sum(reserved_page_count) as sum_reserved_page_count --確保しているエクステントのサイズ
+into dm_db_partition_stats_dump
+from sys.dm_db_partition_stats with(nolock)
+
+--古いデータ削除用
+create index IX_dm_db_partition_stats_dump_collect_date on dm_db_partition_stats_dump(collect_date)
