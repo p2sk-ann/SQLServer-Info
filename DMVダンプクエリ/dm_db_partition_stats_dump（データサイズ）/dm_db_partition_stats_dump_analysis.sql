c1559df134233f0fbba8a3ac72109fd1b9fc1f17
Masaki Hirose <36248899+masaki-hirose@users.noreply.github.com>
2021-09-03 15:46:14 +0900

    Delete DMVダンプクエリ/dm_db_partition_stats_dump（データサイズ） directory

--- DMVダンプクエリ/dm_db_partition_stats_dump（データサイズ）/dm_db_partition_stats_dump_analysis.sql
+++
@@ -1,2 +0,0 @@
-select * from dm_db_partition_stats_dump with(nolock)
-order by collect_date
--- DMVダンプクエリ/dm_db_partition_stats_dump（データサイズ）/dm_db_partition_stats_dump_insert.sql
+++
@@ -1,7 +0,0 @@
-insert into dm_db_partition_stats_dump
-select 
- getdate() as collect_date
-,sum(used_page_count) as sum_used_page_count -- reservedの中で実際にデータが書き込まれているサイズ
-,sum(reserved_page_count) as sum_reserved_page_count --確保しているエクステントのサイズ
-from sys.dm_db_partition_stats with(nolock)
-option (maxdop 1)
