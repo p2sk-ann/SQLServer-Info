2c662d141d819219a9ebde119bf26dfb9be8d0ca
Masaki Hirose <36248899+masaki-hirose@users.noreply.github.com>
2021-09-03 16:00:37 +0900

    Delete DMVダンプクエリ/dm_os_memory_clerks_dump（メモリ使用状況の内訳） directory

--- DMVダンプクエリ/dm_os_memory_clerks_dump（メモリ使用状況の内訳）/dm_os_memory_clerks_dump_analysis.sql
+++
@@ -1,2 +0,0 @@
-select * from dm_os_memory_clerks_dump with(nolock)
-order by collect_date
--- DMVダンプクエリ/dm_os_memory_clerks_dump（メモリ使用状況の内訳）/dm_os_memory_clerks_dump_insert.sql
+++
@@ -1,8 +0,0 @@
-insert into dm_os_memory_clerks_dump
-select 
-getdate() as collect_date
-,type, name, sum(pages_kb) as sum_pages_kb, sum(awe_allocated_kb) as sum_awe_allocated_kb
-from sys.dm_os_memory_clerks with(nolock)
-group by type, name
-order by sum(pages_kb) desc
-option (maxdop 1)
