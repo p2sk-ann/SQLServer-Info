1652eb81cc9fef9756cfafdb0949b27012e1e293
Masaki Hirose <36248899+masaki-hirose@users.noreply.github.com>
2021-09-03 16:04:23 +0900

    Create sys_stats_dump_create.sql

---
+++ DMVダンプクエリ/sys_stats（統計情報更新時間）/sys_stats_dump_create.sql
@@ -0,0 +1,32 @@
+set transaction isolation level read uncommitted
+
+select 
+     getdate() as collect_date
+    ,object_name(s1.object_id) as object_name
+    ,s1.name as statistics_name
+    ,left(colname, len(colname) - 1) as column_list
+	,(case when charindex('|', left(colname, len(colname) - 1), 1) > 1 then 1 else 0 end) as multi_column_flag --1 : 複数列の統計情報
+    ,stats_date(s1.object_id, s1.stats_id) as statsdate --ここで統計情報の更新時間を確認できる
+into sys_stats_dump
+from sys.stats as s1
+inner join sys.stats_columns as sc on s1.object_id = sc.object_id
+    and s1.stats_id = sc.stats_id and stats_column_id = 1
+inner join sys.columns as c on sc.object_id = c.object_id
+    and c.column_id = sc.column_id
+cross apply (
+    select
+      c.name + ' | ' 
+    from
+      sys.stats as s2
+    inner join sys.stats_columns as sc on s2.object_id = sc.object_id
+      and s2.stats_id = sc.stats_id
+    inner join sys.columns as c on sc.object_id = c.object_id
+      and c.column_id = sc.column_id
+    and s1.object_id = s2.object_id and s1.stats_id = s2.stats_id
+	order by stats_column_id
+for xml PATH('')
+) as a(colname)
+where 1=0
+
+--古いデータ削除用
+create index IX_sys_stats_dump_collect_date on sys_stats_dump(collect_date)
