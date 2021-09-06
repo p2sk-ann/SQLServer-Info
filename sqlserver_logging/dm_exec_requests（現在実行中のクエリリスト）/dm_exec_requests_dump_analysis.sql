e7e2544733c83ee8684479289fc48a9e16bdb04f
Masaki Hirose <36248899+masaki-hirose@users.noreply.github.com>
2021-09-06 14:39:37 +0900

    Update dm_exec_requests_dump_analysis.sql

--- sqlserver_logging/dm_exec_requests（現在実行中のクエリリスト）/dm_exec_requests_dump_analysis.sql
+++ sqlserver_logging/dm_exec_requests（現在実行中のクエリリスト）/dm_exec_requests_dump_analysis.sql
@@ -1,2 +1,4 @@
+set transaction isolation level read uncommitted
+
 select * from dm_exec_requests_dump
-where collect_date = '2020-06-29 17:54:11.027'
+where collect_date between '2020-06-29 17:00' and '2020-06-29 18:00'
